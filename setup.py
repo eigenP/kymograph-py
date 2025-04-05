97cf82d909d3d99f4b38cdcfaed8c7fc246477ae
Panos Oikonomou <61333112+eigenP@users.noreply.github.com>
2025-04-04 22:46:03 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup, find_packages
+import os
+
+# Read the README file for a long description.
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name='python_kymograph',  # Package name
     version='0.1.0',          # Version number
     description='A package for generating kymographs from image data.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # This tells PyPI the format of your long description
     author='Panagiotis Oikonomou',       # Your name
     author_email='po2236@columbia.edu',  # Your email
     packages=find_packages(), # Automatically find packages
